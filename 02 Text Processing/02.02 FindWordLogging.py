8aa58c4f6500c1341ad08341e8ccdb68310f1c44
Alireza Nazari <alireza.ginbox@gmail.com>
2022-09-25 14:08:07 +0430

    add new codes

--- 02 Text Processing/02.02 FindWordLogging.py
+++ 02 Text Processing/02.02 FindWordLogging.py
@@ -1,18 +1,26 @@
+import os
+from datetime import datetime
 
+files = os.listdir('input/')
+string = ''
+for file in files:
+    file = open('input/'+file)
+    for line in file:
+        string += line
 
+logFile = open('logs/sys.log', 'a+')
 word = 'Python'
 count = 0
 lines = []
 found = False
 for item in string.splitlines():
     if item.find(word) != -1:
-        if item.find('John:') != -1:
-            lines.append(item)
-            count += 1
-            found = True
+        lines.append(item)
+        count += 1
+        found = True
 if found:
-    print('Number of reputation: ' + str(count))
+    logFile.write('['+str(datetime.now())+'] Info: Count of (' + word + "), " + str(count) + '\n')
     for line in lines:
-        print(line)
+        logFile.write('['+str(datetime.now())+'] Info: line, ' + line + '\n')
 else:
-    print('There are no result!')
+    logFile.write('['+str(datetime.now())+'] Error: There are no result!' + '\n')
